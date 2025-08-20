94c9356067e7760eb85ca7a1c680828df6eb55ca
thecoder1997 <ankit.kharola@sparxsys.com>
2025-08-20 12:21:41 +0530

    Add hiragana and katakana writing tests (#1)
    * new comment added
    
    * new comment
    
    ---------
    
    Co-authored-by: kharola97 <akharola4@gmail.com>

--- src/components/HiraganaChart.jsx
+++ src/components/HiraganaChart.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { HIRAGANA_ALL } from "../data/hiragana";
 import KatakanaChart from "./KatakanaChart";
+// new component has been aadded
+//new comment will be added 
 
 const Section = ({ title, items }) => (
   <div className="mb-8">
